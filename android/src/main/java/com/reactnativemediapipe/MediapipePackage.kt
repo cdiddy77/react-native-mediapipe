e444930666ba4777d62ccfd5d6cbdc44b4b67423
Charles P <charles@parkerfolk.net>
2024-05-04 17:49:45 -0700

    chore: update react-native-vision-camera to v4 (#74)

--- android/src/main/java/com/reactnativemediapipe/MediapipePackage.kt
+++ android/src/main/java/com/reactnativemediapipe/MediapipePackage.kt
@@ -4,7 +4,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-import com.mrousavy.camera.frameprocessor.FrameProcessorPluginRegistry
+import com.mrousavy.camera.frameprocessors.FrameProcessorPluginRegistry
 import com.reactnativemediapipe.objectdetection.ObjectDetectionFrameProcessorPlugin
 import com.reactnativemediapipe.objectdetection.ObjectDetectionModule
 
