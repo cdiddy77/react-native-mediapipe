8e628f0696551c59969a6f16283a0982593e74b3
shackerman <jec.alferez02@gmail.com>
2024-05-09 13:14:42 -0700

    chore: cleaned up my code

--- examples/objectdetection/src/Photo.tsx
+++ examples/objectdetection/src/Photo.tsx
@@ -2,13 +2,13 @@ import type { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import React from "react";
 import { View, Text, Pressable, StyleSheet, Image } from "react-native";
 import type { RootTabParamList } from "./navigation";
-import ImagePicker from 'react-native-image-crop-picker';
+import ImagePicker from "react-native-image-crop-picker";
 
 type Props = BottomTabScreenProps<RootTabParamList, "Photo">;
 
 export const Photo: React.FC<Props> = () => {
   const [screenState, setScreenState] = React.useState<
-  'initial'|'selecting'|"inferring"|"completed"|"error"
+    "initial" | "selecting" | "inferring" | "completed" | "error"
   >("initial");
   const [imagePath, setImagePath] = React.useState<string>();
   const onClickSelectPhoto = () => {
@@ -17,55 +17,48 @@ export const Photo: React.FC<Props> = () => {
       mediaType: "photo",
       width: 300,
       height: 400,
-    }).then(image => {
-      //TODO: run objectdetection on image
-      setImagePath(image.path);
-      setScreenState("completed");
-      console.log(image);
-    }).catch(error => {
-      setScreenState("error");
-    });
+    })
+      .then((image) => {
+        //TODO: run objectdetection on image
+        setImagePath(image.path);
+        setScreenState("completed");
+        console.log(image);
+      })
+      .catch((error) => {
+        setScreenState("error");
+      });
   };
   return (
-    <View style={ styles.root }>
+    <View style={styles.root}>
       {screenState === "initial" && (
-        <Pressable
-        style={styles.selectButton}
-        onPress={onClickSelectPhoto}>
-            <Text style={styles.selectButtonText}>Select a photo</Text>
-          </Pressable>
+        <Pressable style={styles.selectButton} onPress={onClickSelectPhoto}>
+          <Text style={styles.selectButtonText}>Select a photo</Text>
+        </Pressable>
       )}
-        {screenState==='completed'&& (
-    <>
-          <Image source={{uri:imagePath}} style={styles.photo}/>
-          <Pressable
-          style={styles.selectButton}
-          onPress={onClickSelectPhoto}>
+      {screenState === "completed" && (
+        <>
+          <Image source={{ uri: imagePath }} style={styles.photo} />
+          <Pressable style={styles.selectButton} onPress={onClickSelectPhoto}>
             <Text style={styles.selectButtonText}>Select a new photo</Text>
           </Pressable>
-    </>
-    )}
-    {screenState === "error" && (
-      <>
-      <Text style={styles.errorText}>Error! Please try again.</Text>
-        <Pressable
-        style={styles.selectButton}
-        onPress={onClickSelectPhoto}>
+        </>
+      )}
+      {screenState === "error" && (
+        <>
+          <Text style={styles.errorText}>Error! Please try again.</Text>
+          <Pressable style={styles.selectButton} onPress={onClickSelectPhoto}>
             <Text style={styles.selectButtonText}>Select a photo</Text>
           </Pressable>
-      </>
-        
+        </>
       )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  root:{ flex: 1, alignItems: "center", justifyContent: "center"},
-  selectButton: {backgroundColor: "blue", padding: 10, borderRadius: 5 },
-  selectButtonText: {fontSize: 20, color:"white"},
-  photo: {width:300,height:400},
-  errorText: {fontSize: 30, color:"red"}
+  root: { flex: 1, alignItems: "center", justifyContent: "center" },
+  selectButton: { backgroundColor: "blue", padding: 10, borderRadius: 5 },
+  selectButtonText: { fontSize: 20, color: "white" },
+  photo: { width: 300, height: 400 },
+  errorText: { fontSize: 30, color: "red" },
 });
-
-
