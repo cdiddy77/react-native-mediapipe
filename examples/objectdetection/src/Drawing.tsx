6293ffff6f5ca488000ce3489a155b786213c863
Charles Parker <charlesparker@Charless-MacBook-Pro-2.local>
2024-05-18 12:49:07 -0700

    feat: implement object detection on image

---
+++ examples/objectdetection/src/Drawing.tsx
@@ -0,0 +1,138 @@
+import React from "react";
+import {
+  Group,
+  Rect,
+  Skia,
+  Paragraph,
+  TextAlign,
+  matchFont,
+} from "@shopify/react-native-skia";
+import { Platform } from "react-native";
+import {
+  clampToDims,
+  frameRectToView,
+  ltrbToXywh,
+  type DetectionMap,
+  type Dims,
+} from "react-native-mediapipe";
+
+export interface ObjectDetectionFrame {
+  label: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export const ObjectFrame: React.FC<{
+  frame: ObjectDetectionFrame;
+  index: number;
+}> = ({ frame, index }) => {
+  const color = colorNames[index % colorNames.length];
+  if (color === undefined) {
+    throw new Error(`No color found for index ${index}`);
+  }
+  const paragraph = React.useMemo(() => {
+    const textStyle = {
+      backgroundColor: Skia.Color(color),
+      color: Skia.Color(textfromBackground(color)),
+      font: font,
+      fontSize: 24,
+    };
+    const para = Skia.ParagraphBuilder.Make({
+      textAlign: TextAlign.Right,
+    })
+      .pushStyle(textStyle)
+      .addText(` ${frame.label} `)
+      .build();
+    return para;
+  }, [frame.label, color]);
+
+  return (
+    <Group style={"stroke"} strokeWidth={2}>
+      <Paragraph
+        paragraph={paragraph}
+        x={frame.x}
+        y={frame.y}
+        color={color}
+        width={frame.width}
+      />
+      <Rect
+        key={index}
+        x={frame.x}
+        y={frame.y}
+        width={frame.width}
+        height={frame.height}
+        color={color}
+      />
+    </Group>
+  );
+};
+
+const colorNames = [
+  "Coral",
+  "DarkCyan",
+  "DeepSkyBlue",
+  "ForestGreen",
+  "GoldenRod",
+  "MediumOrchid",
+  "SteelBlue",
+  "Tomato",
+  "Turquoise",
+  "SlateGray",
+  "DodgerBlue",
+  "FireBrick",
+  "Gold",
+  "HotPink",
+  "LimeGreen",
+  "Navy",
+  "OrangeRed",
+  "RoyalBlue",
+  "SeaGreen",
+  "Violet",
+];
+
+function textfromBackground(background: string): string {
+  const color = Skia.Color(background);
+  const red = (color[0] ?? 0) * 256;
+  const green = (color[1] ?? 0) * 256;
+  const blue = (color[2] ?? 0) * 256;
+
+  // use the algorithm from https://stackoverflow.com/a/3943023/2197085
+  const text =
+    red * 0.299 + green * 0.587 + blue * 0.114 > 186 ? "black" : "white";
+
+  return text;
+}
+
+const fontFamily = Platform.select({ ios: "Helvetica", android: "sans-serif" });
+const fontStyle = {
+  fontFamily,
+  fontSize: 24,
+};
+const font = matchFont(fontStyle);
+
+export function convertObjectDetectionFrame(
+  detection: DetectionMap,
+  frameSize: { width: number; height: number },
+  viewSize: Dims,
+  mirrored = false
+): ObjectDetectionFrame {
+  const { x, y, width, height } = clampToDims(
+    frameRectToView(
+      ltrbToXywh(detection.boundingBox),
+      frameSize,
+      viewSize,
+      "cover",
+      mirrored
+    ),
+    viewSize
+  );
+  return {
+    label: detection.categories[0]?.categoryName ?? "unknown",
+    x,
+    y,
+    width,
+    height,
+  };
+}
--- examples/objectdetection/src/Photo.tsx
+++ examples/objectdetection/src/Photo.tsx
@@ -3,31 +3,68 @@ import React from "react";
 import { View, Text, Pressable, StyleSheet, Image } from "react-native";
 import type { RootTabParamList } from "./navigation";
 import ImagePicker from "react-native-image-crop-picker";
+import { objectDetectionOnImage, type Dims } from "react-native-mediapipe";
+import { useSettings } from "./app-settings";
+import { Canvas } from "@shopify/react-native-skia";
+import {
+  ObjectFrame,
+  convertObjectDetectionFrame,
+  type ObjectDetectionFrame,
+} from "./Drawing";
 
 type Props = BottomTabScreenProps<RootTabParamList, "Photo">;
+
+const PHOTO_SIZE: Dims = { width: 300, height: 400 };
 
 export const Photo: React.FC<Props> = () => {
   const [screenState, setScreenState] = React.useState<
     "initial" | "selecting" | "inferring" | "completed" | "error"
   >("initial");
+  const { settings } = useSettings();
   const [imagePath, setImagePath] = React.useState<string>();
-  const onClickSelectPhoto = () => {
+  const [objectFrames, setObjectFrames] = React.useState<
+    ObjectDetectionFrame[]
+  >([]);
+  const onClickSelectPhoto = async () => {
     setScreenState("selecting");
-    ImagePicker.openPicker({
-      mediaType: "photo",
-      width: 300,
-      height: 400,
-    })
-      .then((image) => {
-        //TODO: run objectdetection on image
-        setImagePath(image.path);
-        setScreenState("completed");
-        console.log(image);
-      })
-      .catch((error) => {
-        setScreenState("error");
+    try {
+      const image = await ImagePicker.openPicker({
+        mediaType: "photo",
+        width: PHOTO_SIZE.width,
+        height: PHOTO_SIZE.height,
       });
+      const results = await objectDetectionOnImage(
+        image.path,
+        `${settings.model}.tflite`
+      );
+      const frameSize = {
+        width: results.inputImageWidth,
+        height: results.inputImageHeight,
+      };
+      const detections = results.results[0]?.detections ?? [];
+      console.log(
+        JSON.stringify({
+          width: image.width,
+          height: image.height,
+          detections: detections.map((d) => ({
+            bb: d.boundingBox,
+            cat: d.categories[0]?.categoryName,
+          })),
+        })
+      );
+      setObjectFrames(
+        detections.map((r) =>
+          convertObjectDetectionFrame(r, frameSize, PHOTO_SIZE)
+        )
+      );
+      setImagePath(image.path);
+      setScreenState("completed");
+    } catch (e) {
+      console.error(e);
+      setScreenState("error");
+    }
   };
+
   return (
     <View style={styles.root}>
       {screenState === "initial" && (
@@ -37,7 +74,14 @@ export const Photo: React.FC<Props> = () => {
       )}
       {screenState === "completed" && (
         <>
-          <Image source={{ uri: imagePath }} style={styles.photo} />
+          <View style={styles.photoContainer}>
+            <Image source={{ uri: imagePath }} style={styles.photo} />
+            <Canvas style={styles.objectsOverlay}>
+              {objectFrames.map((frame, index) => (
+                <ObjectFrame frame={frame} index={index} key={index} />
+              ))}
+            </Canvas>
+          </View>
           <Pressable style={styles.selectButton} onPress={onClickSelectPhoto}>
             <Text style={styles.selectButtonText}>Select a new photo</Text>
           </Pressable>
@@ -59,6 +103,14 @@ const styles = StyleSheet.create({
   root: { flex: 1, alignItems: "center", justifyContent: "center" },
   selectButton: { backgroundColor: "blue", padding: 10, borderRadius: 5 },
   selectButtonText: { fontSize: 20, color: "white" },
-  photo: { width: 300, height: 400 },
+  photoContainer: { width: PHOTO_SIZE.width, height: PHOTO_SIZE.height },
+  photo: { position: "absolute", top: 0, left: 0, right: 0, bottom: 0 },
+  objectsOverlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
   errorText: { fontSize: 30, color: "red" },
 });
